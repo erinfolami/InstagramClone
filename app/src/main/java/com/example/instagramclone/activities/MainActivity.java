17103f4f8f0d201b401755810307f0b77d1d31c6
erinfolami <emmanuelerinfolami2@gmail.com>
2021-08-15 16:03:07 +0100

    initial commit

---
+++ app/src/main/java/com/example/instagramclone/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.instagramclone.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.instagramclone.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
